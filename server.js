7989118aba38d9bb9f86d767aa16f2bc877dd58b
Dmitrii Bulatov <bulati484@gmail.com>
2024-03-05 20:08:38 +0300

    actualize

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const userRoutes = require("./src/user/routes");
 const app = express();
 const PORT = process.env.SERVER_PORT || 3001;
+const QWERT = process.env.DB_SECRET;
 
 app.use(express.json());
 
@@ -9,7 +10,7 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.use("/api/v1/users", userRoutes);
+app.use(`/${process.env.DB_SECRET}/api/v1/users`, userRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
