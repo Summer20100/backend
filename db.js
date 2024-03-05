e00e464b193f749ee6e3434d810cdfd52a6c580a
Dmitrii Bulatov <bulati484@gmail.com>
2024-03-05 19:55:50 +0300

    finish

--- db.js
+++ db.js
@@ -1,14 +1,12 @@
 require('dotenv').config();
 const { Pool } = require('pg');
 
-SERVER_PORT = process.env.DB_PASS || 5000;
-
 const pool = new Pool({
-    user: 'sudphdzr',
-    host: 'satao.db.elephantsql.com',
-    database: 'sudphdzr',
-    password: 'cLkLQqLpR7YHwt3DWxLdAr2oKwycq_al',
-    port: 5432,
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASS,
+    port: process.env.DB_PORT,
 });
 
 module.exports = pool;
--- src/user/queries.js
+++ src/user/queries.js
@@ -1,4 +1,4 @@
-const getUsers = "SELECT * FROM users LIMIT 10";
+const getUsers = "SELECT * FROM users LIMIT 50";
 const getUserById = "SELECT * FROM users WHERE id=$1";
 const checkEmailExists = "SELECT u FROM users u WHERE u.email = $1";
 const addUser = "INSERT INTO users (name_en,name_ru,position,department,location,email,internal_phone,mobile_phone,actual_location,birthday) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)";
