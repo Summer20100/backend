1b2a25a3744e19aa6a0cff9ec19aef6152d5e1ee
Dmitrii Bulatov <bulati484@gmail.com>
2024-03-06 21:18:55 +0300

    cors correction

--- server.js
+++ server.js
@@ -5,18 +5,27 @@ const PORT = process.env.SERVER_PORT || 3001;
 const cors = require('cors');
 
 app.use(express.json());
-app.use(cors({
-  origin: [
-    "https://b1c849c5-8eba-45e0-8e6a-dfcf73f6745d-00-tblmp5ul2xcl.riker.replit.dev/", 
-    "https://f8d59f01-e4ef-4ba3-9500-b0fe931750ab-00-ylebfj2olvum.worf.replit.dev/",
-  ]
-}));
 
-app.get("/", (req, res) => {
+const whitelist = [
+  'https://b1c849c5-8eba-45e0-8e6a-dfcf73f6745d-00-tblmp5ul2xcl.riker.replit.dev', 
+  'https://f8d59f01-e4ef-4ba3-9500-b0fe931750ab-00-ylebfj2olvum.worf.replit.dev'
+];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+};
+
+app.get("/", cors(corsOptions), (req, res) => {
   res.send("Hello World!");
 });
 
-app.use(`/${process.env.DB_SECRET}/api/v1/users`, userRoutes);
+app.use(`/${process.env.DB_SECRET}/api/v1/users`, cors(corsOptions), userRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
