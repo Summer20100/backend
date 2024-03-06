905c71001dc5a084e63dd7cb64d07f1711f1fbfc
Dmitrii Bulatov <bulati484@gmail.com>
2024-03-06 21:03:16 +0300

    commit

--- server.js
+++ server.js
@@ -2,10 +2,15 @@ const express = require("express");
 const userRoutes = require("./src/user/routes");
 const app = express();
 const PORT = process.env.SERVER_PORT || 3001;
-var cors = require('cors');
+const cors = require('cors');
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: [
+    "https://b1c849c5-8eba-45e0-8e6a-dfcf73f6745d-00-tblmp5ul2xcl.riker.replit.dev/", 
+    "https://f8d59f01-e4ef-4ba3-9500-b0fe931750ab-00-ylebfj2olvum.worf.replit.dev/",
+  ]
+}));
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
