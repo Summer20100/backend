40eb8ab5ed4291e81b85c10e4419532a66a7d41d
Dmitrii Bulatov <bulati484@gmail.com>
2024-03-16 01:42:46 +0300

    some correction

--- src/user/controller.js
+++ src/user/controller.js
@@ -2,13 +2,28 @@ const pool = require("../../db");
 const queries = require("./queries");
 
 const getUsers = async (req, res) => {
+  const { page = 1, size = 50} = req.query;
+  const offset = (page - 1) * size;
+
   try {
-      const result = await pool.query(queries.getUsers);
-      res.status(200).json(result.rows);
+    const countResult = await pool.query('SELECT COUNT(*) FROM users');
+    const totalCount = parseInt(countResult.rows[0].count);
+    const totalPages = Math.ceil(totalCount / size);
+    const result = await pool.query(queries.getUsers, [offset, size]);
+
+    const response = {
+      totalPages,
+      currentPage: parseInt(page),
+      pageSize: parseInt(size),
+      totalCount,
+      data: result.rows
+    };
+
+    res.status(200).json(response);
 
   } catch (error) {
-      console.error('Error executing query', error);
-      res.status(500).json({ error: 'Internal Server Error' });
+    console.error('Error executing query', error);
+    res.status(500).json({ error: 'Internal Server Error' });
   }
 };
 
@@ -26,25 +41,33 @@ const paginationUser = async (req, res) => {
 
 const getUserById = async (req, res) => {
   const id = parseInt(req.params.id);
-  
-  try {
-      const result = await pool.query(queries.getUserById, [id]);
-      res.status(200).json(result.rows);
-  } catch (error) {
-      console.error('Error executing query', error);
-      res.status(500).json({ error: 'Internal Server Error' });
-  }
-};
 
-const getUserByName = async (req, res) => {
-  const name = `%${req.params.name}%`;
-
-  try {
+  if (req.params.id === 'search') {
+    const name = `%${req.query.name}%`;
+    try {
       const result = await pool.query(queries.getUserByName, [name]);
-      res.status(200).json(result.rows);
-  } catch (error) {
+      if (result.rows.length > 0) {
+        res.status(200).json(result.rows);
+      } else {
+        console.error('Error executing query', error);
+        res.status(404).json({ error: `User with name = ${name} does not exist` }); 
+      }
+    } catch {
+      res.status(200).json({ error: `User with name = ${name} does not exist or incorrect name` });
+    }
+  } else {
+    try {
+      const result = await pool.query(queries.getUserById, [id]);
+      if (result.rows.length > 0) {
+        res.status(200).json(result.rows[0]);
+      } else {
+        console.error(`User with id = ${id} does not exist`);
+        res.status(404).json({ error: `User with id = ${id} does not exist` });
+      }
+    } catch (error) {
       console.error('Error executing query', error);
       res.status(500).json({ error: 'Internal Server Error' });
+    }
   }
 };
 
@@ -92,17 +115,26 @@ const addUser = (req, res) => {
 const removeUser = (req, res) => {
   const id = parseInt(req.params.id);
 
-  pool.query(queries.getUserById, [id], (err, results) => {
-    const noUserFound = !results.rows.length;
-    if (noUserFound) {
-      res.status(404).send("User does not exist in database");
-    }
-
+  try {
     pool.query(queries.removeUser, [id], (err, results) => {
-      if (err) throw new Error();
-      res.status(200).send("User removed successfully");
+      if (err) {
+        console.error(`Error removing user with id = ${id}:`, err);
+        res.status(500).send({error: `Error removing user with id = ${id} from the database`});
+        return;
+      };
+  
+      if (results.rowCount === 0) {
+        console.error(`User with id = ${id} does not exist`);
+        res.status(404).send({ error: `User with id = ${id} does not exist` });
+        return;
+      }
+  
+      res.status(200).send({ success: `User with id = ${id} removed successfully` });
     });
-  });
+  } catch {
+    console.error('Error executing query', error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
 };
 
 const updateUser = (req, res) => {
@@ -129,5 +161,4 @@ module.exports = {
   removeUser,
   updateUser,
   paginationUser,
-  getUserByName
 };
--- src/user/routes.js
+++ src/user/routes.js
@@ -6,12 +6,11 @@ const controller = require("./controller");
 const router = Router();
 
 router.get("/from=:from&to=:to", controller.paginationUser);
-router.get("/", controller.getUsers);
+router.get("/", controller.getUsers); // http://localhost:5000/.../api/v1/users?page=<page_number>&size=<page_size>
 router.post("/", controller.addUser);
-router.get("/:id", controller.getUserById);
-router.get("/name=:name", controller.getUserByName);
+router.get("/:id", controller.getUserById);  //  http://localhost:5000/.../api/v1/users/<id>  http://localhost:5000/.../api/v1/users/search?name=<search_query>
 router.put("/:id", controller.updateUser);
-router.delete("/:id", controller.removeUser);
+router.delete("/:id", controller.removeUser); //  http://localhost:5000/.../api/v1/users/<id>
 
 module.exports = router;
 
